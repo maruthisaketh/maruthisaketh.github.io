d809f8172ee12725ef97e2c230b9fe552158838b
maruthisaketh <amsaket1998@gmail.com>
2025-07-10 11:52:10 -0400

    New portfolio

--- src/content/contactContent.jsx
+++ src/content/contactContent.jsx
@@ -3,7 +3,7 @@ const contactDetails = {
     title: "Junior Software Developer",
     summary: "With 4+ years in full-stack development, skilled in Python, Angular, Machine Learning, React",
     organization: "Zolontech Inc.",
-    linkedin: "https://linkedin.com/in/yourprofile",
+    linkedin: "https://www.linkedin.com/in/maruthi-saketh-abbaraju/",
     email: "amsaket1998@gmail.com",
     phone: "+1-(945)-233-6682",
     profileImage: "/images/profile-pic.jpg",
---
+++ src/content/workContent.jsx
@@ -0,0 +1,73 @@
+// workData.js
+
+const workData = [
+  {
+    dates: 'August 2024 — PRESENT',
+    title: 'Junior Software Developer',
+    company: 'ZolonTech Inc',
+    description:
+      `
+      • Engineered a FastAPI-powered platform integrated with Amazon S3, streamlining document management and improving upload/retrieval speeds by 30%.
+
+      • Developed a backend system using Python and FastAPI to interface with Amazon S3, enabling seamless upload and retrieval of application forms and documents.
+
+      • Identified and fixed OpenAI API response issues within Amazon EC2 during query testing, directly improving DAICS data accuracy and reducing retrieval errors by 40%.
+
+      • Designed and launched an internal CMS with Drupal, improving content editing workflow for non-technical stakeholders.`,
+    tags: [
+      'Python', 'FastAPI', 'Amazon S3', 'Amazon EC2', 'Drupal',
+      'Web Development', 'UI/UX'
+    ],
+
+  },
+  {
+    dates: 'June 2024 — August 2024',
+    title: 'Software Development Intern',
+    company: 'ZolonTech Inc',
+    description:
+      `• Directed front-end development for ZolonTech’s website within a team of 8 interns, ensuring responsive UI delivery using WordPress, Bootstrap, and JavaScript.
+       • Developed an intuitive UI integrated with OpenAI’s API to translate natural language into SQL queries, enabling faster and more efficient retrieval of company and contract data from the DAICS API and improving user query resolution time by 45%.`,
+    tags: [
+      'JavaScript', 'Bootstrap', 'WordPress',
+      'UI/UX Design', 'Natural Language Processing', 'SQL'
+    ]
+
+  },
+  {
+    dates: 'June 2019 - June 2022',
+    title: 'Systems Engineer',
+    company: 'Tata Consultancy Services',
+    description:
+      `• Delivered an ML solution using Keras to detect dormant 5G cells, increasing network efficiency and achieving 87.6% model accuracy.
+        • Spearheaded GPU benchmarking initiative, using Vulkan compute API to compare Intel and NVIDIA performance across
+        real-time compute tasks.
+        • Simulated shader workflows via OpenGL and Vulkan to identify bottlenecks, contributing to GPU optimization insights.
+        • Optimized CFD application performance by 5% through CUDA-based parallelization and model tuning on NVIDIA GPUs.
+        • Explored quantum computing models using IBM Qiskit, laying groundwork for future high-performance simulation research.`,
+    tags: [
+      'Keras', 'Machine Learning', '5G',
+      'Vulkan',
+      'OpenGL',
+      'CUDA',
+      'Qiskit', 'Quantum Computing', 'High-Performance Computing',
+    ]
+  },
+  {
+    dates: 'January 2019 - April 2019',
+    title: 'Software Development Intern',
+    company: 'Tata Consultancy Services',
+    description:
+      `• Built a dynamic web portal to visualize network data and transmission stats, improving monitoring insights for engineering teams.
+        • Developed interactive dashboards for cellular activity analytics using modern JavaScript and Bootstrap libraries.
+        • Deployed and managed containerized environments via Docker and Kubernetes, reinforcing DevOps practices.`,
+    tags: [
+      'Web Development', 'Network Monitoring',
+      'JavaScript', 'Bootstrap',
+      'Docker'
+    ]
+
+
+  },
+];
+
+export default workData;
